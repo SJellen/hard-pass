451967dfffe9c0ade0b6e26b0607103643cf39d1
SJellen <sjellen@me.com>
2021-11-17 11:38:11 -0500

    first commit on project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Header.css'
+
+export default function Header() {
+
+    return (
+        <div className="header-container">
+            <h1>Hard Pass</h1>
+        </div>
+    )
+    
+}
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/Main.css'
+
+export default function Main(){
+
+    return (
+        <div className="main-container">
+            <h1>main section</h1>
+        </div>
+    )
+}
