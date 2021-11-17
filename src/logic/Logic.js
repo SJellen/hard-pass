3ede3f0888a47f038f1a33a2a4c4f21015bf4c14
SJellen <sjellen@me.com>
2021-11-17 13:47:30 -0500

    changed to a better randomizer

--- src/logic/Logic.js
+++ src/logic/Logic.js
@@ -1,66 +1,22 @@
 
-let characters = ''
-let passwordLength = ''
 
-function setUpperCase(isUpperCase) {
-    if (isUpperCase) {
-        characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    } 
-    return ''
+function randomPassword(len = 8, minUpper = 0, minLower = 0, minNumber = -1, minSpecial = -1) {
+    let chars = String.fromCharCode(...Array(127).keys()).slice(33),//chars
+        A2Z = String.fromCharCode(...Array(91).keys()).slice(65),//A-Z
+        a2z = String.fromCharCode(...Array(123).keys()).slice(97),//a-z
+        zero2nine = String.fromCharCode(...Array(58).keys()).slice(48),//0-9
+        specials = chars.replace(/\w/g, '')
+    if (minSpecial < 0) chars = zero2nine + A2Z + a2z
+    if (minNumber < 0) chars = chars.replace(zero2nine, '')
+    let minRequired = minSpecial + minUpper + minLower + minNumber
+    let rs = [].concat(
+        Array.from({length: minSpecial ? minSpecial : 0}, () => specials[Math.floor(Math.random() * specials.length)]),
+        Array.from({length: minUpper ? minUpper : 0}, () => A2Z[Math.floor(Math.random() * A2Z.length)]),
+        Array.from({length: minLower ? minLower : 0}, () => a2z[Math.floor(Math.random() * a2z.length)]),
+        Array.from({length: minNumber ? minNumber : 0}, () => zero2nine[Math.floor(Math.random() * zero2nine.length)]),
+        Array.from({length: Math.max(len, minRequired) - (minRequired ? minRequired : 0)}, () => chars[Math.floor(Math.random() * chars.length)]),
+    )
+    return rs.sort(() => Math.random() > Math.random()).join('')
 }
 
-function setLowerCase(isLowerCase) {
-    if (isLowerCase) {
-        characters += 'abcdefghijklmnopqrstuvwxyz'
-    }
-    return ''
-}
-
-function setSymbols(isSymbol) {
-    if (isSymbol) {
-        characters += '!@#$%^&*()<>,.?/[]{}-=_+|/'
-    }
-    return ''
-}
-
-function setNumber(isNumber) {
-    if (isNumber) {
-        characters =+ '0123456789'
-    }
-    return ''
-}
-
-characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()<>,.?/[]{}-=_+|/0123456789'
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-function passwordCharacters() {
-    let password = ''
-    if (characters.length) {
-        for (let i = 0; i < passwordLength; i++) {
-            password += characters[getRandomInt(0, characters.length - 1)]
-        }
-        characters = ''
-        passwordLength = 0
-        return password
-    }
-
-}
-
-export function setPasswordLength(length) {
-    passwordLength = length
-    return passwordLength
-}
-
-export function generatePassword(passWordProps, pwdLength) {
-    const { uppercase, lowercase, symbols, numbers} = passWordProps
-    setPasswordLength(pwdLength)
-    setUpperCase(uppercase)
-    setLowerCase(lowercase)
-    setSymbols(symbols)
-    setNumber(numbers)
-    const password = passwordCharacters()
-    return password
-}
+console.log(randomPassword(27, 5, 10, 5, 15))
