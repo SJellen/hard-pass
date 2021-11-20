748d4a48fad3451380c61bd1f41344ea085c65dc
SJellen <sjellen@me.com>
2021-11-20 12:05:16 -0500

    added close logic on slidedown

--- src/components/SlideDown.js
+++ src/components/SlideDown.js
@@ -9,7 +9,11 @@ export default function SlideDown() {
 
     return (
         <div className="slide-down-container" style={{display: sliderOpen ? '' : 'none'}}>
-            <div className="close-container"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="close-button"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg></div>
+            <div className="close-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="close-button" onClick={() => setSliderOpen(false)}>
+                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/>
+                </svg>
+            </div>
         </div>
     )
 }
